97217e8bd19e282833b3412371645dbb61b94a24
Nathan Hamblen <nathan@technically.us>
2009-11-29 16:41:57 -0500

    android plugin version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val android = "android-plugin" % "android-plugin" % "0.1.3"
+  val android = "android-plugin" % "android-plugin" % "0.2"
 }
