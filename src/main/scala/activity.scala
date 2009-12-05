10400ce722bc4de70cde437a8edce33a6cf09ddc
Nathan Hamblen <nathan@technically.us>
2009-12-05 17:12:51 -0500

    event and group names in simple_list_item_2

--- src/main/scala/activity.scala
+++ src/main/scala/activity.scala
@@ -2,7 +2,7 @@ package meetup.example
  
 import android.app.ListActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
  
 class MainActivity extends ListActivity {
   override def onCreate(savedInstanceState: Bundle) {
@@ -18,12 +18,20 @@ class MainActivity extends ListActivity {
     val consumer = Consumer("72DA10F33DB36B11DA502251ED135E76","F6805ED5DB63D7AFE9BF0506B6430CF2")
     val at = Token("6beed634b161ce2ff6f3963ea6287b9e","3ce7bbbe6a5b200b3117d83e5196b6e9")
     val cli = OAuthClient(consumer, at)
-    val h = new Http
-    val events = h(cli(Events.member_id("7230113")) ># (
-      Response.results andThen { _ flatMap Event.name }
-    )) toArray
+    implicit val h = new Http
+    val (events, _) = cli.call(Events.member_id("7230113"))
+    val data = for (e <- events; name <- Event.name(e); group <- Event.group_name(e)) yield {
+      val map = new java.util.HashMap[String, String]
+      map.put("name", name)
+      map.put("group", group)
+      map
+    }
     
-    setListAdapter(new ArrayAdapter(this,
-      android.R.layout.simple_list_item_1, events));
+    setListAdapter(new SimpleAdapter(this, 
+      java.util.Arrays.asList(data.toArray: _*),
+      android.R.layout.simple_list_item_2,
+      Array("name", "group"),
+      Array(android.R.id.text1, android.R.id.text2)
+    ))
   }
 }
