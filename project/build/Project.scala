f70dc0d59696c88f4a9ada87541b00a883a60ce1
Nathan Hamblen <nathanh@meetup.com>
2009-12-18 10:50:01 -0500

    use android 1.6 sdk

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo) {    
   override def androidSdkPath = Path.fromFile("/usr/local/android-sdk-mac/")
-  override def androidPlatformName="android-2.0"
+  override def androidPlatformName="android-1.6"
 
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.6.5-SNAPSHOT"
