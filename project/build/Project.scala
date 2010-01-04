580978446c858aca965586cea153c7286e872246
Nathan Hamblen <nathanh@meetup.com>
2010-01-04 10:17:39 -0500

    dispatch 0.6.5

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,7 +5,7 @@ class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo)
   override def androidPlatformName="android-1.6"
 
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.6.5-SNAPSHOT"
+  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.6.5"
   override def proguardOption = """
     |-keep class dispatch.** { 
     |  public scala.Function1 *();
