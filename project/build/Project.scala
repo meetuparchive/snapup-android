6c4e9a3f86b8f3df471dce4d716017ff4ae51336
Nathan Hamblen <nathan@technically.us>
2010-01-04 00:34:09 -0500

    dispatch 0.6.5 final

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo) {    
-  override def androidSdkPath = Path.fromFile("/usr/local/android-sdk/")
+  override def androidSdkPath = Path.fromFile("/usr/local/android-sdk-mac/")
   override def androidPlatformName="android-1.6"
 
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.6.5-SNAPSHOT"
+  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.6.5"
   override def proguardOption = """
     |-keep class dispatch.** { 
     |  public scala.Function1 *();
