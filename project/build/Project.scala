88e6bc93e3ebb1e84fdcd83d823e80db3c7f960a
Nathan Hamblen <nathan@technically.us>
2010-01-24 20:59:30 -0500

    rm provided httpclient

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,7 +5,6 @@ class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo)
 
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.7.0-beta1"
-  val http_client_1 = "org.apache.httpcomponents" % "httpclient" % "4.0.1" % "provided"
 
   override def proguardOption = """
     |-keep class dispatch.** { 
