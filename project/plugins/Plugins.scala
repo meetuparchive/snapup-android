4ea6eaaab1aaa0f71e6cbb74676f92f7a1f59c9b
Nathan Hamblen <nathan@technically.us>
2010-01-24 20:59:07 -0500

    update to in-progress plugin

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val android = "org.scala-tools.sbt" % "android-plugin" % "0.3"
+  val android = "org.scala-tools.sbt" % "android-plugin" % "0.4"
 }
