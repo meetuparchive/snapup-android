e4a678656f0ae26732fa7798d6044d16363124e7
Nathan Hamblen <nathan@technically.us>
2010-06-08 23:16:07 -0400

    listview test

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,12 +1,13 @@
 import sbt._
 
-class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo) with MarketPublish with posterous.Publish {    
+class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo) 
+    with MarketPublish with TypedResources with posterous.Publish {    
   def androidPlatformName="android-1.6"
   def keyalias = "snapup"
   override def keystorePath = Path.userHome / "meetup.keystore"
 
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.7.2"
+  lazy val meetup = "net.databinder" %% "dispatch-meetup" % "0.7.4"
 
   override def proguardOption = """
     |-keep class dispatch.** { 
@@ -19,4 +20,16 @@ class SnapupProject(info: ProjectInfo) extends AndroidProject(info: ProjectInfo)
     |  public ** product();
     |}
 """.stripMargin
+}
+
+trait TypedResources extends AndroidProject {
+  import scala.xml._
+  lazy val generateTypedResources = task {
+    (mainResPath ** "*.xml").get.foreach { path =>
+      val xml = XML.loadFile(path.asFile)
+      val android_ns = "http://schemas.android.com/apk/res/android"
+      xml \\ "ListView" \\ "@{%s}id".format(android_ns) foreach println
+    }
+    None
+  }
 }
